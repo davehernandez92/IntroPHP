34bbfd208703065fa4fb7f7c10d79e7503f3e157
davehernandez92 <davehgal@gmail.com>
2024-03-19 06:09:36 -0300

    object progr

--- 17_class.php
+++ 17_class.php
@@ -1,3 +1,22 @@
 <?php 
   include_once './includes/header.php'; 
 ?>
+
+<?php 
+  class Client {
+    // Declaring property
+    public $name;
+
+    public function __construct($client)
+    {
+      $this->name = $client;
+    }
+
+    public function show_name() {
+      echo 'The name is ' . $this->name;
+    }
+  }
+
+  $client = new Client('David');
+  $client->show_name();
+?>
